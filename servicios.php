66f6c8f4d71f02e7e7d011521693420adac0ac99
Fernanda <f.alfaro.sagua@gmail.com>
2021-08-19 00:04:04 -0400

    Vicuna

--- servicios.php
+++ servicios.php
@@ -11,7 +11,7 @@
     <link
       href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900"
       rel="stylesheet"
-    />
+    /> 
     <link
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
